55211f9f05bad69d83bb862a30b4f69b8926ef06
Sencerhan Taşar <sencerhan76@gmail.com>
2024-08-22 23:25:14 +0300

    fundamental migrations

---
+++ database/migrations/2024_08_22_185458_create_ads_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->integer("status")->default(1);
+            $table->integer("sort_order")->default(0);
+            $table->integer("currency_id")->default(0);
+            $table->decimal("price", 8, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ads');
+    }
+};
